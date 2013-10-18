fbf6c8d39b5d90cc55f5a555dc1cb5af7c78dfab
Jeremy G. Kahn <jeremy@trochee.net>
2013-10-17 17:36:55 -0700

    fixed up parse-forest code

--- src/main/scala/net/trochee/parse/ParseForest.scala
+++ src/main/scala/net/trochee/parse/ParseForest.scala
@@ -1,30 +1,28 @@
 package net.trochee.parse
 
-
-class ParseForest(s:String) {
-	abstract class Span {
-		def start:Position
-		def end:Position
-		def likelihood:Probability
-		def spanString:String = "(" + start + "->" + end + ")"
-		def likelihoodString:String = "[" + likelihood + "]"
-	}
-	class Terminal (val start:Position, val end:Position) extends Span {
-		override def toString():String = s.subSequence(start, end) + spanString
-		val prob = 1.0
-		def likelihood = prob
-	}
-	class NonTerminal(val label:Label, val spans:Seq[Span]) extends Span {
-		override def toString():String = label + "(" + spans + ") " + spanString
-		def start:Position = spans.head.start
-		def end:Position = spans(spans.length - 1).end
-		val prob = 1.0
-		
-		lazy val likelihood = prob * spans.map(_.likelihood).foldLeft(prob)(_ * _)
-	}
-
+class ParseForest(s: String) {
+  abstract class Span {
+    def start: Position
+    def end: Position
+    def likelihood: Probability
+    def label: Label
+    def spanString: String = "(" + start + "->" + end + ")"
+    def likelihoodString: String = "[" + likelihood + "]"
+  }
+  class Terminal(val label: Label, val start: Position, val end: Position) extends Span {
+    override def toString(): String = s.subSequence(start, end) + spanString
+    val prob = 1.0
+    def likelihood = prob
+  }
+  class NonTerminal(val label: Label, val spans: Seq[Span]) extends Span {
+    override def toString(): String = label + "(" + spans + ") " + spanString
+    def start: Position = spans.head.start
+    def end: Position = spans(spans.length - 1).end
+    val prob = 1.0
+    lazy val likelihood = prob * spans.map(_.likelihood).foldLeft(prob)(_ * _)
+  }
 }
 object ParseForest {
-  def apply(s:String) = new ParseForest(s)
+  def apply(s: String) = new ParseForest(s)
 }
  
