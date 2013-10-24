8df2d5adc2e91322e0a0a307baac47251a92e18a
Jeremy G. Kahn <jeremy@trochee.net>
2013-10-24 09:22:10 -0700

    include more types in parse package

--- src/main/scala/net/trochee/parse/package.scala
+++ src/main/scala/net/trochee/parse/package.scala
@@ -1,7 +1,8 @@
 package net.trochee
 
 package object parse {
-	type Position = Int
-	type Label = String
-	type Probability = Double
+  type Token = String
+  type Position = Int
+  type Label = String
+  type Probability = Double
 }
